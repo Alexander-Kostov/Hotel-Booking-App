38583b1067f6a0ecf62f28814f2fa7b2af977916
Alexander <alex3747@abv.bg>
2024-03-28 20:44:23 +0200

    Adding Booking Response class

---
+++ lakeside-hotel/src/main/java/LakesideHotel/lakesidehotel/response/BookingResponse.java
@@ -0,0 +1,40 @@
+package LakesideHotel.lakesidehotel.response;
+
+import LakesideHotel.lakesidehotel.model.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookingResponse {
+
+    private Long id;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private String guestName;
+
+    private String guestFullName;
+
+    private int numOfAdults;
+
+    private int numOfChildren;
+
+    private int totalNumOfGuest;
+
+    private String bookingConfirmationCode;
+
+    private RoomResponse room;
+
+    public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+}
